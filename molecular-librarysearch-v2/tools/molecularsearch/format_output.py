7a9db91a00b1034a32651dda172ab385d778abda
asmirn1 <smirnov09al@gmail.com>
2023-10-22 11:09:08 -0400

    Format the GNPS output

--- molecular-librarysearch-v2/tools/molecularsearch/format_output.py
+++ molecular-librarysearch-v2/tools/molecularsearch/format_output.py
@@ -7,6 +7,28 @@ from spectrum.utils import expand_directories
 from typing import List
 
 
+def filter_by_library(data: pd.DataFrame) -> pd.DataFrame:
+    common_columns = ['#Scan#', 'Charge', 'SpecMZ', 'SpectrumFile', 'p-value']
+    annotation_columns = ['Peptide', 'ExactMass', 'SharedPeaks', 'SpectrumID', 'Score', 'MassDiff', 'Protein',
+                          'mzErrorPPM', 'FalseDiscoveryRate', 'IonMode', 'Instrument', 'Adduct', 'INCHI']
+
+    filtered_rows = []
+    for _, annotation_group in data.groupby(by=['#Scan#']):
+        row = annotation_group.iloc[0][common_columns]
+        unique_libraries = annotation_group['LibraryName'].unique()
+        for library in unique_libraries:
+            library_group = annotation_group[annotation_group['LibraryName'] == library] if isinstance(library, str) else annotation_group
+            sorted_group = library_group.sort_values(by='MQScore', ascending=False)
+            for column in annotation_columns:
+                # library_name = rename_nist_library(library)
+                row[f'{library}: {column}'] = sorted_group.iloc[0][column]
+        filtered_rows.append(row)
+
+    # filtered_data = pd.concat(filtered_rows, ignore_index=True)
+    filtered_data = pd.DataFrame(data=filtered_rows)
+    return filtered_data
+
+
 def add_library_info(annotations: pd.DataFrame, library_filename: str):
     library = read_mgf(library_filename, id_field='SPECTRUMID')
     library_name = basename(library_filename)
@@ -31,13 +53,16 @@ def format_output(annotations_filename: str, library_filenames: List[str], outpu
     annotations = pd.read_csv(annotations_filename, header=0, sep='\t')
     annotations.rename(mapper={
         'LibrarySpectrumID': 'SpectrumID',
-        'Compound_Name': 'CompoundName',
+        'CompoundName': 'Peptide',
         'ParentMassDiff': 'MassDiff',
-        'LibSearchSharedPeaks': 'SharedPeaks'
+        'LibSearchSharedPeaks': 'SharedPeaks',
+        'MQScore': 'Score'
     }, axis=1, inplace=True)
 
     for library_filename in library_filenames:
         add_library_info(annotations, library_filename)
+
+    annotations = filter_by_library(annotations)
 
     annotations.to_csv(output_filename, index=False, sep='\t')
 

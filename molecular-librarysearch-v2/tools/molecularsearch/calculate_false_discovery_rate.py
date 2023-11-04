8551cbf06ff33d946d2cd0593722cb9965671927
asmirn1 <smirnov09al@gmail.com>
2023-11-04 13:29:24 -0400

    Update the FDR formula

--- molecular-librarysearch-v2/tools/molecularsearch/calculate_false_discovery_rate.py
+++ molecular-librarysearch-v2/tools/molecularsearch/calculate_false_discovery_rate.py
@@ -22,7 +22,7 @@ def calculate_false_discovery_rate(input_filename: str, output_filename: str):
                 num_target_libraries += 1
 
         false_discovery_rate = num_decoy_libraries / (num_target_libraries + num_decoy_libraries)\
-            if num_target_libraries > 0 and num_decoy_libraries > 0 else None
+            if num_target_libraries > 0 or num_decoy_libraries > 0 else None
         annotations.loc[selection, 'FalseDiscoveryRate'] = false_discovery_rate
 
     annotations.to_csv(output_filename, index=False, sep='\t')
